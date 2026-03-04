a1ab876a6e09cc9feead62e904009ac2ccfe9f1a
Vannia-LeVe <vlevevannia@gmail.com>
2026-03-03 19:37:12 -0700

    modificaciones

---
+++ src/main/java/mx/itson/aa_uc2_268467/Busquedas.java
@@ -0,0 +1,14 @@
+package mx.itson.aa_uc2_268467;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Vanni
+ */
+public class Busquedas {
+    
+}
